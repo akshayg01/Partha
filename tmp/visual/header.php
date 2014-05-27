eadabda14b7bf3f4b944ed789b140b03ebb4c475
akshayg01 <akshayg01@gmail.com>
2014-05-27 14:08:20 +0530

    Signed-off-by: akshayg01 <akshayg01@gmail.com>

--- tmp/visual/header.php
+++
@@ -1,9 +0,0 @@
-<STYLE>
-body {
-        background: url(images/background.jpg) no-repeat center center fixed;
-        -webkit-background-size: cover;
-        -moz-background-size: cover;
-        -o-background-size: cover;
-        background-size: cover;
-}
-</STYLE>
